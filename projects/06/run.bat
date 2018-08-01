cdd9b1a11a22b8c2b0412871ea42c4c8e710bc84
HideyukiFUKUHARA <fukuhara.hideyuki@gmail.com>
2018-08-01 18:27:13 +0900

    added run.bat

---
+++ projects/06/run.bat
@@ -0,0 +1,15 @@
+
+
+call Assembler.bat ./add/Add.asm
+
+call Assembler.bat ./max/Max.asm
+
+call Assembler.bat ./max/MaxL.asm
+
+call Assembler.bat ./pong/Pong.asm
+
+call Assembler.bat ./pong/PongL.asm
+
+call Assembler.bat ./rect/Rect.asm
+
+call Assembler.bat ./rect/RectL.asm
